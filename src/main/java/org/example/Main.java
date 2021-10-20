5009545c0126daacb40e9fed8cf07ed2bb75468f
Yaroslav Voitiuk <yaroslav.voitiuk@gmail.com>
2021-10-20 14:05:59 +0300

    task fixed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,17 @@
 package org.example;
 
-
 public class Main {
+    public static final int START_VAL = 1;
+    public static final int MAX_VAl = 3;
+
     public static void main(String[] args) throws Exception {
         Shop account = new Shop();
-        Thread thread1 = new Thread(null, account::transferCash, "Магазин 1");
-        Thread thread2 = new Thread(null, account::transferCash, "Магазин 2");
-        Thread thread3 = new Thread(null, account::transferCash, "Магазин 3");
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread3.join();
-        thread2.join();
-        thread1.join();
+
+        for (int i = START_VAL; i <= MAX_VAl; i++){
+            Thread thread = new Thread(null, account::transferCash, "Магазин " + i);
+            thread.start();
+            thread.join();
+        }
         account.calculate();
 
     }
--- src/main/java/org/example/Shop.java
+++ src/main/java/org/example/Shop.java
@@ -1,22 +1,18 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 import java.util.concurrent.atomic.LongAdder;
 
 
 public class Shop  {
-   private static final int ARR_SIZE = 5;
-   private static final int MAX_RANDOM_VALUE = 100;
+   private static final int ARR_SIZE = 7;
+   private static final int INCREASE_VALUE = 100;
    LongAdder bank = new LongAdder();
 
    public void transferCash(){
       System.out.println(Thread.currentThread().getName() + " переводит деньги");
-      List<Integer> cash = new ArrayList<>();
+      int [] cash = new int[ARR_SIZE];
       for (int i = 0; i < ARR_SIZE; i++){
-         cash.add(new Random().nextInt(MAX_RANDOM_VALUE));
+         cash[i] = (int) (Math.random()*INCREASE_VALUE);
       }
       for (Integer i:cash) {
          bank.add(i);
